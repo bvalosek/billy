e818b503b414bce20cf9cc3a646a5c6f094bfc8f
Brandon Valosek <bvalosek@gmail.com>
2017-05-05 09:26:57 -0500

    more test cases

--- test/get-arguments.test.js
+++ test/get-arguments.test.js
@@ -1,9 +1,18 @@
+/* eslint no-unused-vars: 0 */
+
 const ga = require('../lib/getArguments.js');
 const test = require('tape');
 
 test('getArguments: arguments for a function', t => {
-  t.deepEqual(ga(function (a, b, c) { return c; }), [ 'a', 'b', 'c' ], 'found args for anon function');
-  t.deepEqual(ga(function (c, b, a) { return a; }), [ 'c', 'b', 'a' ], 'found args for anon function (order preserved)');
+  t.deepEqual(ga(function (a, b, c) {  }), [ 'a', 'b', 'c' ], 'found args for anon function');
+  t.deepEqual(ga(function (/* a, */ a, b /* d, */, c) {  }), [ 'a', 'b', 'c' ], 'found args for anon function (comments)');
+  t.deepEqual(ga(function (c, b, a) {  }), [ 'c', 'b', 'a' ], 'found args for anon function (order preserved)');
+  t.end();
+});
+
+test('getArguments: arguments for a function (nested)', t => {
+  t.deepEqual(ga(function (a, b, c) { function f(x, y) { return z => z; } }), [ 'a', 'b', 'c' ], 'found args for anon function (with other nested crap)');
+  t.deepEqual(ga(function (c, b, a) {  }), [ 'c', 'b', 'a' ], 'found args for anon function (order preserved)');
   t.end();
 });
 
@@ -14,7 +23,15 @@ test('getArguments: arguments for an arrow function', t => {
 });
 
 test('getArguments: arguments for a class', t => {
-  t.deepEqual(ga(class T { constructor(a, b, c) { return c; } }), [ 'a', 'b', 'c' ], 'found args for class');
-  t.deepEqual(ga(class T { constructor(c, b, a) { return a; } }), [ 'c', 'b', 'a' ], 'found args for class (in order)');
+  t.deepEqual(ga(class T { constructor(a, b, c) { } }), [ 'a', 'b', 'c' ], 'found args for class');
+  t.deepEqual(ga(class T { constructor(c, b, a) { } }), [ 'c', 'b', 'a' ], 'found args for class (in order)');
+  t.end();
+});
+
+test('getArguments: arguments for a class, ctor not first', t => {
+  t.deepEqual(ga(class T { foo(x, y, z) {  } constructor(a, b, c) { } }), [ 'a', 'b', 'c' ], 'found args for class after method');
+  t.deepEqual(ga(class T { static foo(x, y, z) {  } constructor(a, b, c) { } }), [ 'a', 'b', 'c' ], 'found args for class after static method');
+  t.deepEqual(ga(class T { get foo() {  } constructor(a, b, c) { } }), [ 'a', 'b', 'c' ], 'found args for class after getter');
+  t.deepEqual(ga(class T { set foo(v) {  } constructor(a, b, c) { } }), [ 'a', 'b', 'c' ], 'found args for class after setter');
   t.end();
 });
