cdbe4cf990bba54108c635e6d4832a0f2dc72c39
Brandon Valosek <bvalosek@gmail.com>
2017-05-05 08:08:34 -0500

    failing tests: arrow function in getArgs

--- test/get-arguments.test.js
+++ test/get-arguments.test.js
@@ -7,6 +7,12 @@ test('getArguments: arguments for a function', t => {
   t.end();
 });
 
+test('getArguments: arguments for an arrow function', t => {
+  t.deepEqual(ga((a, b, c) => c), [ 'a', 'b', 'c' ], 'found args for anon function');
+  t.deepEqual(ga((c, b, a) => a), [ 'c', 'b', 'a' ], 'found args for anon function (order preserved)');
+  t.end();
+});
+
 test('getArguments: arguments for a class', t => {
   t.deepEqual(ga(class T { constructor(a, b, c) { return c; } }), [ 'a', 'b', 'c' ], 'found args for class');
   t.deepEqual(ga(class T { constructor(c, b, a) { return a; } }), [ 'c', 'b', 'a' ], 'found args for class (in order)');
