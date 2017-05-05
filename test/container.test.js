0799e5610c5aa3634d810e33658d38e9dd4d4dcb
Brandon Valosek <bvalosek@gmail.com>
2017-05-05 07:49:04 -0500

    local injections

--- test/container.test.js
+++ test/container.test.js
@@ -8,6 +8,32 @@ test('Container: works with no deps', t => {
   const foo = c.new(T);
   t.ok(foo instanceof T, 'class with no deps is instantiated');
 
+  const S = Symbol('S');
+  const factory = () => S;
+  t.strictEqual(c.call(factory), S, 'call with no deps works');
+
+  t.end();
+});
+
+test('Container: works with locals', t => {
+  const c = new Container();
+
+  const factory = function (b, a) { return [ a, b ]; };
+
+  t.throws(() => c.call(factory), 'throws when missing deps');
+
+  const A = Symbol('A');
+  const B = Symbol('B');
+  t.deepEqual(c.call(factory, { a: A, b: B }), [ A, B ], 'locals passed for call()');
+
+  class T { constructor(b, a) { this.a = a; this.b = b; } }
+
+  t.throws(() => c.new(T), 'throws when missing deps');
+
+  const instance = c.new(T, { a: A, b: B });
+  t.strictEqual(instance.a, A, 'injected dep');
+  t.strictEqual(instance.b, B, 'injected dep');
+
   t.end();
 });
 
@@ -34,5 +60,9 @@ test('Container: basic value deps', t => {
     t.strictEqual(d2, B, 'registerd value was injected');
   }
 
+  t.end();
+});
+
+test('Container: basic class deps', t => {
   t.end();
 });
